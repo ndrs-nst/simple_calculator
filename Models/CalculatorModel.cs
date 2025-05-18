45d0531e4ed99edbf14b24f73d04d194d3fe2727
nisatorn <nisatorn@ndrsolution.com>
2025-05-18 20:37:05 +0700

    Add model class

---
+++ Models/CalculatorModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calculator.Models
+{
+    internal enum MathOperator
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+
+    internal class CalculatorModel
+    {
+        public double LeftOperand { get; set; }
+        public double RightOperand { get; set; }
+        public MathOperator Operator { get; set; }
+
+        public double Calculate()
+        {
+            switch (Operator)
+            {
+                case MathOperator.Add:
+                    return LeftOperand + RightOperand;
+                case MathOperator.Subtract:
+                    return LeftOperand - RightOperand;
+                case MathOperator.Multiply:
+                    return LeftOperand * RightOperand;
+                case MathOperator.Divide:
+                    if (RightOperand != 0)
+                        return LeftOperand / RightOperand;
+                    else
+                        throw new DivideByZeroException("Cannot divide by zero.");
+                default:
+                    throw new InvalidOperationException("Invalid operation.");
+            }
+        }
+    }
+}
